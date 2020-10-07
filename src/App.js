74c38f1e463deec7dfa222efc3ae4a731a71cf66
suprim12 <suprimofficial@yahoo.com>
2020-10-07 23:29:22 +0545

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { NavBar, Button, Container, Input, Spacer } from "suprim-ui";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar>
+        <NavBar.Container>
+          <NavBar.Menu>
+            <NavBar.Link>
+              <a href="/" className="active">
+                Home
+              </a>
+            </NavBar.Link>
+            <NavBar.Link>
+              <a href="/">About</a>
+            </NavBar.Link>
+          </NavBar.Menu>
+        </NavBar.Container>
+      </NavBar>
+      <Container>
+        <Spacer></Spacer>
+        <Input type="text" placeholder="Full Name" label="Full Name" />
+        <Input type="email" placeholder="Email" label="Email Address" />
+        <Input
+          type="password"
+          placeholder="Password"
+          label="Password"
+          passwordtoggle
+        />
+        <Button type="submit" variation="primary">
+          Submit
+        </Button>
+        <Spacer></Spacer>
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { SuprimThemeProvider, theme } from "suprim-ui";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SuprimThemeProvider theme={theme.dark}>
+      <App />
+    </SuprimThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
